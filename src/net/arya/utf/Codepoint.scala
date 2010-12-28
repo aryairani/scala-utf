2ff8637bfa29451de7081322d5b3827dce9d501c
Arya Irani <arya.irani@gmail.com>
2010-12-28 02:05:28 -0500

    Added Codepoint.scala

---
+++ src/net/arya/utf/Codepoint.scala
@@ -0,0 +1,11 @@
+package net.arya.utf
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: arya
+ * Date: 12/28/10
+ * Time: 12:05 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+object Codepoint
